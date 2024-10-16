9fe1853a988eafaf3e5d46abb39ace20a6a1c4e7
AngelCrispin <angelcrispinhuamani@gmail.com>
2024-10-16 16:06:34 -0500

    Feat(Structure): Structure Project Finished

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// function MyButton({ title }: { title: string }) {
+//     return (
+//       <button>{title}</button>
+//     );
+//   }
+  
+export default function Header() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      {/* <MyButton title="I'm a button" /> */}
+    </div>
+  );
+}
+
